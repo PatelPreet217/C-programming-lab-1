45baca2641fae3b728c8ff26c0d72625321e191e
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-04 15:35:12 +0530

    Add files via upload

---
+++ grams to kgs.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+    int main() {
+   float grams,kgs;
+   printf("Enter weight in grams: ");
+   scanf("%f",&grams);
+   kgs=grams/1000;
+   printf("kgs= %f",kgs);
+   return 0;
+    }
