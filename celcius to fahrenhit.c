0f713655021a2c4db3749b4c9daf9963d61c73f5
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-04 15:49:41 +0530

    Add files via upload

---
+++ celcius to fahrenhit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+    int main() {
+   float celcius,fahrenhit;
+   printf("celcius: ");
+   scanf("%f",&celcius);
+   fahrenhit=(celcius*9/5)+32;
+   printf("fahrenhit= %f\n",fahrenhit);
+   return 0;
+    }
