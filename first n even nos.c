9b37bbd1f0aef056d27ddeae46daf7983f5c22cb
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-06 22:36:13 +0530

    Update first n even nos.c

--- first n even nos.c
+++ first n even nos.c
@@ -3,9 +3,10 @@ int main(){
 int n;
 printf("Enter n: ");
 scanf("%d",&n);
-printf("Numbers from 1 to n: \n");
+printf("Numbers from 1 to %d: \n",n);
 for(int i=2;i<=n;i+=2){
 printf("%d\n",i);
 }
 return 0;
 }
+
