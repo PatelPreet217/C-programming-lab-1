39b095f397f59b49fc54f5955a6ef08bfac687b0
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-06 22:53:48 +0530

    Add files via upload

---
+++ print your name n times.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int n,fact=1;
+printf("Enter n: ");
+scanf("%d",&n);
+printf("Print Preet %d times \n",n);
+for(int i=1;i<=n;i++){
+printf("Preet\n");
+}
+return 0;
+}
