9b23ae2b3e72dab13a3428d066e72c2cf1f2868d
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-06 21:46:03 +0530

    Add files via upload

--- net salary.c
+++ net salary.c
@@ -1,22 +1,11 @@
-#include<stdio.h>
-int main()
-{
-float gross_salary,allowances,deductions,net_salary;
-printf("Enter gross salary:");
-scanf("%f",&gross_salary);
-if(gross_salary>10000)
-{
-allowances=0.1*gross_salary;
-deductions=0.03*gross_salary;
-}
-else if(gross_salary>5000)
-{
-allowances=0.07*gross_salary;
-deductions=0.02*gross_salary;
-}
-net_salary=gross_salary + allowances - deductions;
-printf("allowances=%2f\n",allowances);
-printf("deductions=%2f\n",deductions);
-printf("net salary=%2f\n",net_salary);
+#include <stdio.h>
+int main(){
+float netsales,grosssalary,allowance,deduction;
+printf("Gross salary: ");
+scanf("%f",&grosssalary);
+allowance=grosssalary/10;
+deduction=grosssalary*3/100;
+netsales=grosssalary+allowance-deduction;
+printf("Net salary = %2f\n",netsales);
 return 0;
 }
