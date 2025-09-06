1544f9448ffe84545bfde1c204a2141e46c1fa79
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-06 21:16:59 +0530

    Add files via upload

---
+++ area and perimeter of sqaure.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+float A,L,P;
+printf("Enter Length: ");
+scanf("%f",&L);
+A=L*L;
+P=4*L;
+printf("Area of square = %2f\n",A);
+printf("Perimeter of square = %2f",P);
+}
