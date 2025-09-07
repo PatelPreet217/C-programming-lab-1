e67046b071bf6096a22e1b3cb85d5733f2565058
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-07 23:26:13 +0530

    Add files via upload

---
+++ reverse a number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+int n,reversedNumber=0,lastDigit;
+printf("Enter n: ");
+scanf("%d",n);
+while(n>0){
+   lastDigit=n%10;
+   reversedNumber=reversedNumber*10+lastDigit;
+   n=n/10;
+}
+printf("Reversed number is: %d",reversedNumber);
+return 0;
+}
+
+
