1a9683f63ee0a0783ae7d4cfcc675c747a9d194b
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-14 19:16:39 +0530

    Create Print 4th 7th and 9th elements of array.c

---
+++ Print 4th 7th and 9th elements of array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+int arr[10],i;
+printf("Enter 10 elements: ");
+
+for( i=0;i<10;i++){  
+    scanf("%d",&arr[i]);  
+}  
+printf("4th element is: %d\n",arr[3]);  
+printf("7th element is: %d\n",arr[6]);  
+printf("9th element is: %d",arr[8]);  
+  
+
+return 0;
+
+}
+
