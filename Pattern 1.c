233ce5ca796d91ffb2d4f08e53cf5ae6bedfa808
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-14 22:14:46 +0530

    Create Pattern 1.c

---
+++ Pattern 1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=3;i++){
+        printf("1 %d",i);
+        printf("\n");
+    }
+    
+    for(i=1;i<=3;i++){
+        printf("2 %d",i);
+        printf("\n");
+    }
+
+    return 0;
+}
