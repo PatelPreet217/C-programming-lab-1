cf682e7702f5d0289513e0d9bf286005fab1ee00
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-16 19:20:47 +0530

    Create Reverse an array.c

---
+++ Reverse an array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    
+    int arr[5],temp;
+    printf ( " Enter 5 elments: ");
+    for ( int i=0; i< 5; i++ ) {
+        scanf ( " %d", &arr[i] );
+    }
+    
+    for ( int i=0; i<5/2; i++ ) {
+        temp = arr[i];
+        arr[i] = arr[4-i];
+        arr[4-i] = temp;
+    }
+    
+    printf ( " Reversed array is: ");
+    for ( int i=0; i<5; i++ ) {
+        printf ( "%d ",arr[i] );
+    }
+    return 0;
+}
