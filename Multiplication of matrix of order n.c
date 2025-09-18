dffb9c54352bbcb644d8cbae683af997f1bddd32
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-18 21:30:09 +0530

    Update Multiplication of matrix of order n.c

--- Multiplication of matrix of order n.c
+++ Multiplication of matrix of order n.c
@@ -1,36 +1,52 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    printf("Enter order of matrix: ");
-    scanf("%d", &n);
+    int a,b,c,d,i,j,k;
+    printf("Enter rows and colums of matrix 1: ");
+    scanf("%d %d",&a,&b);
+    printf("Enter rows and colums of matrix 2: ");
+    scanf("%d %d",&c,&d);
+    
+    int mat1[a][b],mat2[c][d],mat3[a][d];
+    printf("Enter elments of matrix 1: \n");
+    for(int i=0; i<a;i++){
+        for(int j=0;j<b;j++){
+            scanf("%d",&mat1[i][j]);
+        }
+    }
+    
+    printf("Enter elments of matrix 2: \n");
+    for(int i=0; i<c;i++){
+        for(int j=0;j<d;j++){
+            scanf("%d",&mat2[i][j]);
+        }
+    }
+    
+    if(b!=c){
+    printf("Multiplication is not possible.");
+    return 0;
+}
 
-    int mat1[n][n], mat2[n][n], mat3[n][n];
-
-    printf("Enter elements of matrix 1:\n");
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            scanf("%d", &mat1[i][j]);
-
-    printf("Enter elements of matrix 2:\n");
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            scanf("%d", &mat2[i][j]);
-
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            mat3[i][j] = 0;
-
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            for (int k = 0; k < n; k++)
-                mat3[i][j] += mat1[i][k] * mat2[k][j];
-
-    printf("Multiplication of the two matrices is:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++)
-            printf("%d ", mat3[i][j]);
-        printf("\n");
+      for(int i=0; i<a;i++){
+        for(int j=0;j<d;j++){
+           mat3[i][j]=0;
+        }
     }
 
+    for(int i=0;i<a;i++){
+        for(int j=0;j<d;j++){
+            for(int k=0;k<b;k++){
+                mat3[i][j]+=mat1[i][k]*mat2[k][j];
+            }
+        }
+    }
+    
+     printf("Multipliation of martix 1 and matrix 2 is: \n");
+    for(int i=0;i<a;i++){
+        for(int j=0;j<d;j++){
+            printf("%d ",mat3[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
+}
