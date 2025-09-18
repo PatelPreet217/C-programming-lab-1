3688320cb1bd9dae1875c3dd3ed9ef7cacfa9187
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-18 11:44:25 +0530

    Update Pattern 1.c

--- Pattern 1.c
+++ Pattern 1.c
@@ -3,15 +3,10 @@
 int main()
 {
     int i,j;
-    for(i=1;i<=3;i++){
-        printf("1 %d",i);
-        printf("\n");
+    for(i=1;i<=2;i++){
+        for(int j=1;j<=3;j++){
+        printf("%d %d\n",i,j);
+        }       
     }
-    
-    for(i=1;i<=3;i++){
-        printf("2 %d",i);
-        printf("\n");
-    }
-
     return 0;
 }
