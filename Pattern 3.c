341ca0cd6bc9eb89dabd6d077c3810e2cac3a30a
PatelPreet217 <pvpatel200883@gmail.com>
2025-09-25 13:02:12 +0530

    Update Pattern 3.c

--- Pattern 3.c
+++ Pattern 3.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int i, j, k, n;
-    printf("Enter n: ");
-    scanf("%d", &n);
+    int i,j,k;
 
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= n; j++) {
-            for (k = 1; k <= n; k++) {
+    for (i =1; i <=2; i++) {
+       for (j =1; j <=2; j++) {
+            for (k =1; k <=2; k++) {
                 printf("%d %d %d\n", i, j, k);
-            }
         }
+    }
     }
     return 0;
 }
