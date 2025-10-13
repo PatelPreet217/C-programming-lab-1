aff0097f201b812fbd9742f71353942d9c004ab5
PatelPreet217 <pvpatel200883@gmail.com>
2025-10-13 14:48:52 +0530

    Update check palindrome.c

--- check palindrome.c
+++ check palindrome.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 int main() {
-int n,digit,reversed=0;
+int n,a,digit,reversed=0;
 printf("Enter n:" );
 scanf("%d",&n);
+
+a=n;
 while(n>0){
 digit=n%10;
 reversed=reversed*10+digit;
 n/=10;
 }
 printf("Reversed number is: %d\n",reversed);
-if(reversed==n){
+if(reversed==a){
    printf("Number is palindrome");
 }else{
     printf("Number is not palindrome");
 }
 return 0;
 }
+
