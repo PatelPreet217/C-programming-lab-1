c61ba7de81f2ab94d00e3a9ab1878be75e83403b
PatelPreet217 <pvpatel200883@gmail.com>
2025-11-07 17:08:20 +0530

    Create calculator using function.c

---
+++ calculator using function.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int sum( int a, int b);
+int difference( int a, int b);
+int product( int a, int b);
+float division( int a, int b);
+
+int main() {
+    int a,b;
+    printf("Enter a and b: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Sum of %d and %d is: %d\n", a, b, sum(a, b));
+    printf("Difference of %d and %d is: %d\n", a, b, difference(a, b));
+    printf("Product of %d and %d is: %d\n", a, b, product(a, b));
+    printf("Division of %d and %d is: %f\n", a, b, division(a, b));
+
+    return 0;
+}
+
+int sum( int a, int b){
+    return (a+b);
+
+}
+
+int difference( int a, int b){
+    return (a-b);
+
+}
+
+int product( int a, int b){
+    return (a*b);
+
+}
+
+float division( int a, int b){
+    return ((float)a/b);
+
+}
+    
+    
